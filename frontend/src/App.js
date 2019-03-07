1582cbd4644892b6be3a1dbc329c424c7090ea20
hraya <hraya@lssu.edu>
2019-03-06 23:19:00 -0600

    Refactored Code
    Refactored Appjs, Callbackjs, and NavBar
    
    added SecuredRoutejs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import NavBar from './components/NavBar.js';
 import Questions from './components/Questions.js';
 import Question from './Question/Question.js'
+import Callback from './components/Callback';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <NavBar />
        <Route exact path='/' component={Questions} />
        <Route exact path='/question/:questionId' component={Question} />
+       <Route exact path='/callback' component={Callback} />
       </div>
     );
   }

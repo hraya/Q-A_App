fedb2f2d1e1d454f3ae8a08551fc2759d9964eb9
hraya <hraya@lssu.edu>
2019-03-11 10:55:21 -0500

    Can now submit answers
    Modified the Submit Answer Component and Question Component
    
    need to work on keeping it logged in after a refresh

--- frontend/src/Question/SubmitAnswer.js
+++ frontend/src/Question/SubmitAnswer.js
@@ -1,52 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
-import auth0Client from '../components/Auth,js';
+import auth0Client from '../components/Auth.js';
 
 class SubmitAnswer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            answer:'',
-         };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      answer: ''
+    };
+  }
 
-    updateAnswer(value) {
-        this.setState({
-            answers:value,
-        });
-    }
+  updateAnswer(value) {
+    this.setState({
+      answer: value
+    });
+  }
 
-    submit() {
-        this.props.submitAnswer(this.state.answer);
+  submit() {
+    this.props.submitAnswer(this.state.answer);
 
-        this.setState({
-            answer:'',
-        });
-    }
+    this.setState({
+      answer: ''
+    });
+  }
 
-    render() { 
-        if(!auth0Client,isAuthenticated()) return null:
-        return ( 
-            <>
-                <div className="form-group text-center">
-                    <label htmlFor="exampleInputEmail1">Answer:</label>
-                    <input
-                        type="text"
-                        onChange={(e) => {this.updateAnswer(e.target.value)}}
-                        className="form-control"
-                        placeholder="Share your answer."
-                        value={this.state.answer}
-                    />
-                </div>
-            <button
-            className="btn btn-primary"
-            onClick={() => {this.submit()}}>
-            Submit
-            </button>
-            <hr className="my-4" />
-            </>
-         );
-    }
+  render() {
+    if (!auth0Client.isAuthenticated()) return null;
+    return (
+      <>
+        <div className="form-group text-center">
+          <label htmlFor="exampleInputEmail1">Answer:</label>
+          <input
+            type="text"
+            onChange={e => {
+              this.updateAnswer(e.target.value);
+            }}
+            className="form-control"
+            placeholder="Share your answer."
+            value={this.state.answer}
+          />
+        </div>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            this.submit();
+          }}
+        >
+          Submit
+        </button>
+        <hr className="my-4" />
+      </>
+    );
+  }
 }
- 
+
 export default withRouter(SubmitAnswer);
